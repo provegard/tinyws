0433f482b90a81525f6be0fabee272d640cdf5ed
Per Rovegård <per@rovegard.se>
2017-03-12 10:44:17 +0100

    Adding a text-success websocket test

--- src/test/java/com/programmaticallyspeaking/tinyws/WebSocketTest.java
+++ src/test/java/com/programmaticallyspeaking/tinyws/WebSocketTest.java
@@ -5,25 +5,52 @@ import org.java_websocket.drafts.Draft;
 import org.java_websocket.drafts.Draft_17;
 import org.java_websocket.framing.Framedata;
 import org.java_websocket.handshake.ServerHandshake;
+import org.mockito.Mockito;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.CountDownLatch;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.*;
 
 public class WebSocketTest extends ClientTestBase {
 
+    private boolean useEchoHandler;
+
     private URI createURI() throws URISyntaxException {
         return new URI("ws://" + host + ":" + port);
+    }
+
+    @BeforeMethod
+    public void init() {
+        useEchoHandler = false;
+    }
+
+    @Override
+    protected WebSocketHandler createHandler() {
+        return useEchoHandler ? new EchoHandler() : super.createHandler();
     }
 
     private void sendIncorrectFrame() throws Exception {
         SimpleClient cl = new SimpleClient(createURI());
         cl.sendRawData(new byte [] { (byte)112 });
         cl.waitUntilClosed();
+    }
+
+    private SimpleClient sendText(String text) throws Exception {
+        SimpleClient cl = new SimpleClient(createURI());
+        cl.send(text);
+        cl.sendClose(1001);
+        cl.waitUntilClosed();
+        return cl;
     }
 
     private void sendClose() throws Exception {
@@ -60,8 +87,17 @@ public class WebSocketTest extends ClientTestBase {
         verify(handler, never()).onClosedByServer(anyInt(), anyString());
     }
 
+    @Test
+    public void Correct_frame_should_result_in_echo_response() throws Exception {
+        useEchoHandler = true;
+        SimpleClient cl = sendText("hello world");
+        assertThat(cl.messages).containsExactly("hello world");
+    }
+
     static class SimpleClient extends org.java_websocket.client.WebSocketClient {
         private CountDownLatch closeLatch = new CountDownLatch(1);
+
+        List<String> messages = new ArrayList<>();
 
         void sendRawData(byte[] data) {
             ((DraftThatAllowsUsToSendBogusData) getConnection().getDraft()).setDataToSend(data);
@@ -81,6 +117,7 @@ public class WebSocketTest extends ClientTestBase {
         }
 
         public void onMessage(String message) {
+            messages.add(message);
         }
 
         public void onClose(int code, String reason, boolean remote) {
@@ -88,6 +125,12 @@ public class WebSocketTest extends ClientTestBase {
         }
 
         public void onError(Exception ex) {
+        }
+
+        void sendClose(int code) {
+            // We just want to send the close frame and let the remote close the connection, otherwise we might
+            // not receive an echo response in time.
+            sendRawData(new byte [] { (byte)136, 2, (byte) ((code & 0xff00) >>> 8), (byte) (code & 0xff) });
         }
     }
 
